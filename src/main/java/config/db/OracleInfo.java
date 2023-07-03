486f87142f575f2907746f707c347a6194122423
Goooony <gunhee5161@naver.com>
2023-07-03 09:57:28 +0900

    [add] second commit

--- src/main/java/config/db/OracleInfo.java
+++ src/main/java/config/db/OracleInfo.java
@@ -4,5 +4,5 @@ public class OracleInfo {
 	public static String _driver = "oracle.jdbc.driver.OracleDriver";
 	public static String _url = "jdbc:oracle:thin:@localhost:1521:xe";
     public static String _user = "HELLOUSER";	
-    public static String _password = "HELLOUSER";
+    public static String _password = "5161";
 }
